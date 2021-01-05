668f4934ea99e7a251f3c514039da1f2385015d6
Wojciech <wojciechrozwalka@gmail.com>
2021-01-05 19:36:08 +0100

    finish auth and roles

--- src/main/java/javer/springbootregistrationloginsecurity/service/UserServiceImpl.java
+++ src/main/java/javer/springbootregistrationloginsecurity/service/UserServiceImpl.java
@@ -5,11 +5,15 @@ import javer.springbootregistrationloginsecurity.model.User;
 import javer.springbootregistrationloginsecurity.repository.UserRepository;
 import javer.springbootregistrationloginsecurity.web.dto.UserRegistrationDto;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import java.util.Arrays;
+import java.util.Collection;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -30,7 +34,18 @@ public class UserServiceImpl implements UserService {
     }
 
     @Override
-    public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
-        return null;
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(username);
+        if (user == null) {
+            throw new UsernameNotFoundException("Invalid username or password");
+        }
+        return new org.springframework.security.core.userdetails
+                .User(user.getEmail(), user.getPassword(), mapRolesToAuthorities(user.getRoles()));
+    }
+
+    private Collection<? extends GrantedAuthority> mapRolesToAuthorities(Collection<Role> roles) {
+        return roles.stream()
+                .map(role -> new SimpleGrantedAuthority(role.getName()))
+                .collect(Collectors.toList());
     }
 }
