b2cb8b7508295c84352cf7a7ffa383ee520523b5
Wojciech <wojciechrozwalka@gmail.com>
2021-01-05 09:12:08 +0100

    init commit

---
+++ src/main/java/javer/springbootregistrationloginsecurity/SpringbootRegistrationLoginSecurityApplication.java
@@ -0,0 +1,13 @@
+package javer.springbootregistrationloginsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootRegistrationLoginSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootRegistrationLoginSecurityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/javer/springbootregistrationloginsecurity/SpringbootRegistrationLoginSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package javer.springbootregistrationloginsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootRegistrationLoginSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
